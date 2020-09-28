26ada65f4f9d1ea06ed3ce3033d8d8e2f45f42a2
Laurens Brosens <brlaurens@gmail.com>
2020-09-28 17:13:31 +0200

    getInput methode

--- Input.cs
+++ Input.cs
@@ -6,5 +6,18 @@ namespace AgeTranslator
 {
     class Input
     {
+        static public void getInput()
+        {
+            int jaar = 1900;
+            int maand = 12;
+            int dag = 27;
+            Console.WriteLine("In welk jaar ben je geboren?");
+            jaar = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("In welke maand ben je geboren?");
+            maand = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Op welke dag ben je geboren?");
+            dag = Convert.ToInt32(Console.ReadLine());
+            AgeCalculation.berekening(dag, maand, jaar);
+        }
     }
 }
