840fbec096ba8c8f70fe00c52df33c4b53d850d6
Arvo cant <aro.vant@student.ap.be>
2020-09-28 17:03:42 +0200

    Aanmaking berekening method

--- AgeCalculation.cs
+++ AgeCalculation.cs
@@ -6,5 +6,14 @@ namespace AgeTranslator
 {
     class AgeCalculation
     {
+        public int Dag { get; set; }
+        public int Maand { get; set; }
+        public int Jaar { get; set; }
+
+        static void berekening(int Dag, int Maand, int Jaar)
+        {
+            DateTime huidigetijd = new DateTime(Jaar, Maand, Dag);
+            
+        }
     }
 }
