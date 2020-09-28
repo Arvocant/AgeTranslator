718e43a467818a0d605e5c89c46b25833aa1ba01
Laurens Brosens <brlaurens@gmail.com>
2020-09-28 17:26:40 +0200

    Output toegevoegd

---
+++ Output.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeTranslator
+{
+    class Output
+    {
+        public static void giveOutput(TimeSpan tijd)
+        {
+            Console.WriteLine(tijd);
+        }
+    }
+}
