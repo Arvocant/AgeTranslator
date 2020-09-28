19a8f462550af3f8741c7a9a8445e4587c46121f
Arvo cant <aro.vant@student.ap.be>
2020-09-28 16:26:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgeTranslator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
